234121aab1809d514b234a621d3a363d2043a233
Satyawan Jadhav <jadhavsatyawan1999@gmail.com>
2025-12-16 15:08:01 +0530

    Commiting Login Page add page objects

---
+++ src/test/java/pageObjects/Login.java
@@ -0,0 +1,13 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class Login extends BasePage
+{
+
+	public Login(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
