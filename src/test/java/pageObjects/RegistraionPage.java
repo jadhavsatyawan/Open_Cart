f3044c41db2fca077529e961607542ec41ee7526
Satyawan Jadhav <jadhavsatyawan1999@gmail.com>
2025-12-16 16:42:24 +0530

    Changes Registration Page

--- src/test/java/pageObjects/RegistraionPage.java
+++ src/test/java/pageObjects/RegistraionPage.java
@@ -4,39 +4,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class RegistraionPage extends BasePage
-{
+public class RegistraionPage extends BasePage {
 
-	public RegistraionPage(WebDriver driver) 
-	{
+	public RegistraionPage(WebDriver driver) {
 		super(driver);
 	}
 
-	//Locators
-	
-	//id
+	// Locators
+
+	// id
 	@FindBy(id = "input-firstname")
 	WebElement firstName;
-	
-	//xpath with Multiple Attributes
+
+	// xpath with Multiple Attributes
 	@FindBy(xpath = "//input[@name='lastname'][@placeholder='Last Name']")
 	WebElement lastName;
-	
-	//xpath with OR Operators
+
+	// xpath with OR Operators
 	@FindBy(xpath = "//input[@name='email' or @placeholder='E-Mail']")
 	WebElement email;
-	
-	//xpath with AND operator
+
+	// xpath with AND operator
 	@FindBy(xpath = "//input[@type='password' and @name='password']")
 	WebElement password;
-	
-	//xpath following-sibling
+
+	// xpath following-sibling
 	@FindBy(xpath = "//input[@type='hidden']/following-sibling::input[@type='checkbox']")
 	WebElement newsLetter;
-	
+
 	@FindBy(xpath = "//input[@name='agree']")
 	WebElement privacyPolicy;
-	
-	
-	
+
 }
